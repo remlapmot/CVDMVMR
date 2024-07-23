9b02d6755bbb66ec44a88f2ca4eff119e6cf40b3
Tom Palmer <remlapmot@hotmail.com>
2024-07-23 14:53:23 +0100

    Update redo-figures.R

--- redo-figures.R
+++ redo-figures.R
@@ -2,7 +2,8 @@ library(readxl)
 library(dplyr)
 if (!requireNamespace('ggforestplot')) remotes::install_github("NightingaleHealth/ggforestplot")
 library(ggforestplot)
-library(labelled)
+library(ggplot2)
+library(ggforce)
 
 # Figure 6 ----
 
@@ -16,8 +17,6 @@ dat <- dat |>
 dat <- dat |> 
   mutate(logorse = (logorupp - logorlow)/(2 * 1.96))
 
-var_label(dat$estimator) <- "Estimator"
-
 dat <- dat |> 
   mutate(Estimator = estimator)
 
@@ -30,6 +29,7 @@ fig6 <- forestplot(
   se = logorse,
   logodds = TRUE,
   colour = Estimator,
+  shape = Estimator,
   xlab = "Odds ratio with 95% CI (IVW and MVMR estimates)"
 ) + 
   ggplot2::theme_minimal() + 
@@ -37,3 +37,38 @@ fig6 <- forestplot(
 fig6
 ggplot2::ggsave('img/fig6.pdf')
 ggplot2::ggsave('img/fig6.tiff')
+
+# Figure 5 ----
+
+df <- read_excel('redo-figures.xlsx', sheet = 'fig5')
+
+df <- df |> 
+  mutate(logor = log(oddsratio),
+         logorlow = log(orlowci),
+         logorupp = log(oruppci))
+
+df <- df |> 
+  mutate(logorse = (logorupp - logorlow)/(2 * 1.96))
+
+# Forestplot
+
+fig5 <- forestplot(
+  df = df,
+  name = Exposure,
+  estimate = logor,
+  se = logorse,
+  logodds = TRUE,
+  colour = Estimator,
+  shape = Estimator,
+  xlab = "Odds ratio with 95% CI (IVW and MVMR estimates)"
+) + 
+  ggplot2::theme_minimal() + 
+  ggplot2::scale_colour_grey(start = 0, end = .6) +
+  ggforce::facet_col(
+    facets = ~Outcome,
+    scales = "free_y",
+    space = "free"
+  )
+fig5
+ggplot2::ggsave('img/fig5.pdf')
+ggplot2::ggsave('img/fig5.tiff')
